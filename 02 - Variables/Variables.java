c7da223b33ae131378f94364dba8798d3e43afe6
rajusaa <santoshkumar.skr1@gmail.com>
2025-02-06 11:00:00 +0800

    variables section

---
+++ 02 - Variables/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 10;      //this is a number variable
+		String str = "santosh"; //this is a string variable
+		Boolean b = true ;     //this is a boolean variable
+		System.out.println(number); 
+		System.out.println(str);
+		System.out.println(b);
+	}
+
+}
