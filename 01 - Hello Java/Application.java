14ee98ae335f27940ecfebdbfb267ff949e6d603
rajusaa <santoshkumar.skr1@gmail.com>
2025-02-06 11:27:33 +0800

    updated

--- 01 - Hello Java/Application.java
+++ 01 - Hello Java/Application.java
@@ -1,9 +1,9 @@
-
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Hello,Java!");
+		System.out.print("Hello,again!");
 	}
 
 }
